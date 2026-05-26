9448a75c48536487c327a7a1dd601677059b91ba
Yared Acosta <nifemare14@gmail.com>
2026-05-26 07:49:23 -0500

    Node modules eliminado

--- src/calculator.js
+++ src/calculator.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
-    throw new Error('No se puede dividir entre cero.');
+    throw new Error('No se puede dividir entre CERO.');
   }
 
   return a / b;
